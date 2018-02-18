94282028d018d19a68ef385ebc57ac407d3e722c
TheTripleV <vasistavovveti@gmail.com>
2018-02-18 16:06:40 -0500

    Updated Auto Template Methods
    -goStraight, goStraightSS, and autoPID have been updated to ca20cf2 in
    my branch in 2018CaMOElot

--- src/org/usfirst/frc/team365/robot/Robot.java
+++ src/org/usfirst/frc/team365/robot/Robot.java
@@ -295,38 +295,63 @@ public class Robot extends IterativeRobot {
 	int turnOnTargetCount = 0;
 
 	public void goStraight(double ticks, double setPoint, double power) {
-		if (Math.abs(getEncoderMax())> ticks) {
-			driveRobot(0,0);
-			driveStraight.reset();
+		if (newPID) {
 			resetEncoders();
-			autoStep++;
-		} else {
-				driveStraight.setSetpoint(setPoint);
-				driveStraight.enable();
+			driveStraight.reset();
+			driveStraight.setSetpoint(setPoint);
+			driveStraight.enable();
+			newPID = false;
+		}
 
-			driveRobot(power + driveStraightCorrection.correctionValue, power - driveStraightCorrection.correctionValue);
+		if (Math.abs(getEncoderMax()) > ticks) {
+			driveRobot(0, 0);
+			driveStraight.reset();
+			autoTimer.reset();
+			autoStep++;
+			newPID = true;
+		} else {
+			driveRobot(power + driveStraightCorrection.correctionValue,
+					power - driveStraightCorrection.correctionValue);
 		}
 	}
 
+	public void turnToAngle(double angle, double maxPower, double tolerance) {
+		if (newPID) {
+			turnRobot.setAbsoluteTolerance(tolerance);
+		}
+		turnToAngle(angle, maxPower);
+	}
+
 	public void turnToAngle(double angle, double maxPower) {
+		if (newPID) {
+			resetEncoders();
+			turnRobot.reset();
+			turnRobot.setSetpoint(angle);
+			turnRobot.setOutputRange(-Math.abs(maxPower), Math.abs(maxPower));
+			turnRobot.enable();
+			turnOnTargetCount = 0;
+			newPID = false;
+		}
+
 		if (turnRobot.onTarget()) {
 			turnOnTargetCount++;
 		}
+
 		if (turnOnTargetCount > 3) {
 			resetEncoders();
-			driveRobot(0,0);
+			driveRobot(0, 0);
 			turnOnTargetCount = 0;
-			autoStep++;
 			turnRobot.reset();
+			autoTimer.reset();
+			autoStep++;
+			newPID = true;
 		} else {
-			turnRobot.setSetpoint(angle);
-			turnRobot.enable();
-			driveRobot(turnRobotCorrection.correctionValue * Math.abs(maxPower), -turnRobotCorrection.correctionValue* Math.abs(maxPower));
+			driveRobot(turnRobotCorrection.correctionValue, -turnRobotCorrection.correctionValue);
 		}
 	}
 
 	public void turnToAngle(double angle) {
-		turnToAngle(angle, 1);
+		turnToAngle(angle, .6);
 	}
 
 	public void resetEncoders() {
@@ -335,7 +360,144 @@ public class Robot extends IterativeRobot {
 	}
 
 	public double getEncoderMax() {
-		return (distanceL.getRaw() + distanceR.getRaw()) / 2.0;
+		return distanceL.getRaw() > distanceR.getRaw() ? distanceL.getRaw() : distanceR.getRaw();
+	}
+
+	public double getStraightPower() {
+		return (driveOutputLeft + driveOutputRight) / 2.0;
+	}
+
+	public void pause(double seconds) {
+		if (autoTimer.get() > seconds) {
+			autoStep++;
+			autoTimer.reset();
+		}
+	}
+
+	public void halfTurnLeft(double angle, double power) {
+		if (Math.abs(navX.getYaw() - angle) < 3) {
+			driveRobot(0, 0);
+			autoStep++;
+		} else {
+			driveRobot(0, power);
+		}
+	}
+
+	public void halfTurnRight(double angle, double power) {
+		if (Math.abs(navX.getYaw() - angle) < 3) {
+			driveRobot(0, 0);
+			autoStep++;
+		} else {
+			driveRobot(power, 0);
+		}
+	}
+
+	/**
+	 * 
+	 * Below is a modification to goStraight that will ramp up and ramp down the
+	 * speed of the bot to minimize tipping. The 4 doubles at the start of
+	 * goStraightSS need to be tweaked for goodness - especially the last 2.
+	 */
+
+	
+	public void goStraightSS(double ticks, double setPoint, double maxPower) {
+		double deltaSpeedIncrease = .01;
+		double deltaSpeedDecrease = .01;
+		double distAwayFromTargetToStartBraking = 24 * INCHES_TO_ENCTICKS;
+		double maxOKBrakingPower = .3;
+
+		if (newPID) {
+			resetEncoders();
+			driveStraight.reset();
+			driveStraight.setSetpoint(setPoint);
+			driveStraight.enable();
+			newPID = false;
+		}
+
+		if (Math.abs(getEncoderMax()) > ticks) {
+			driveRobot(0, 0);
+			driveStraight.reset();
+			autoTimer.reset();
+			autoStep++;
+			newPID = true;
+		} else {
+			if (ticks - Math.abs(getEncoderMax()) < distAwayFromTargetToStartBraking
+					&& getStraightPower() > maxOKBrakingPower) {
+				driveRobot(getStraightPower() - deltaSpeedDecrease + driveStraightCorrection.correctionValue,
+						getStraightPower() - deltaSpeedDecrease - driveStraightCorrection.correctionValue);
+			}
+
+			else if (getStraightPower() < maxPower) {
+				driveRobot(getStraightPower() + deltaSpeedIncrease + driveStraightCorrection.correctionValue,
+						getStraightPower() + deltaSpeedIncrease - driveStraightCorrection.correctionValue);
+			} else {
+				driveRobot(maxPower + driveStraightCorrection.correctionValue,
+						maxPower - driveStraightCorrection.correctionValue);
+			}
+		}
+
+	}
+
+	double turnSum = 0;
+	double offYaw = 0;
+	double lastOffYaw = 0;
+	
+	public void autoPIDTurn(double desiredYaw) {
+		double currentYaw = navX.getYaw();
+		double offYaw = desiredYaw - currentYaw;
+
+		// if driving near 180 need to correct offYaw
+		if (offYaw > 180)
+			offYaw = offYaw - 360;
+		else if (offYaw < -180)
+			offYaw = offYaw + 360;
+
+		// initialize values during first loop
+		if (newPID) {
+			turnSum = 0;
+			newPID = false;
+			lastOffYaw = offYaw;
+			turnOnTargetCount = 0;
+			driveRobot(0, 0);
+		} else {
+
+			// re-zero the error sum when turn past yaw setpoint
+			if (offYaw * lastOffYaw <= 0) {
+				turnSum = 0;
+			}
+
+			// determine if within yaw tolerance
+			if (offYaw > 2 || offYaw < -2) {
+				// only add to error sum when close to target value
+				if (offYaw < 20 && offYaw > -20) {
+					if (offYaw > 0)
+						turnSum = turnSum + 0.01;
+					else
+						turnSum = turnSum - 0.01;
+				}
+				// calculate new correction value
+				double newPower = turnP * offYaw + turnSum + turnD * (offYaw - lastOffYaw);
+
+				// limit output power
+				if (newPower > 0.6)
+					newPower = 0.6;
+				else if (newPower < -0.6)
+					newPower = -0.6;
+				driveRobot(newPower, -newPower);
+			}
+			// if robot is within yaw tolerance stop robot and increase onCount
+			else {
+				turnOnTargetCount++;
+				if (turnOnTargetCount > 3) {
+					turnOnTargetCount = 0;
+					autoStep++;
+					driveRobot(0, 0);
+					newPID = true;
+				}
+
+			}
+			lastOffYaw = offYaw;
+		}
 	}
 
 }
