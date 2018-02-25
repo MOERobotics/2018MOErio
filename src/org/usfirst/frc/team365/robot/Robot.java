faa2904f4de7ac3c3c31bec2e6da1b7690625149
Kevin West <kevok93@gmail.com>
2018-02-25 14:07:18 -0500

    Un-auto-formatting robot.java

--- src/org/usfirst/frc/team365/robot/Robot.java
+++ src/org/usfirst/frc/team365/robot/Robot.java
@@ -12,16 +12,8 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.kauailabs.navx.frc.AHRS;
 
-import edu.wpi.first.wpilibj.AnalogInput;
-import edu.wpi.first.wpilibj.CounterBase.EncodingType;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.PIDController;
-import edu.wpi.first.wpilibj.SPI;
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.CounterBase.*;
 
 public class Robot extends TimedRobot {
 	// Motors
@@ -29,46 +21,22 @@ public class Robot extends TimedRobot {
 	// immediately after the object is constructed.
 	// Longer answer: Anonymous default constructor for anonymous class implementing
 	// given class
-	TalonSRX driveLA = new TalonSRX(12) {
-		{
-			setNeutralMode(NeutralMode.Brake);
-		}
-	};
-	TalonSRX driveLB = new TalonSRX(13) {
-		{
-			setNeutralMode(NeutralMode.Brake);
-		}
-	};
-	TalonSRX driveLC = new TalonSRX(14) {
-		{
-			setNeutralMode(NeutralMode.Brake);
-		}
-	};
-	TalonSRX driveRA = new TalonSRX(1) {
-		{
-			setNeutralMode(NeutralMode.Brake);
-		}
-	};
-	TalonSRX driveRB = new TalonSRX(2) {
-		{
-			setNeutralMode(NeutralMode.Brake);
-		}
-	};
-	TalonSRX driveRC = new TalonSRX(3) {
-		{
-			setNeutralMode(NeutralMode.Brake);
-		}
-	};
+	TalonSRX driveLA = new TalonSRX(12) {{ setNeutralMode(NeutralMode.Brake); }};
+	TalonSRX driveLB = new TalonSRX(13) {{ setNeutralMode(NeutralMode.Brake); }};
+	TalonSRX driveLC = new TalonSRX(14) {{ setNeutralMode(NeutralMode.Brake); }};
+	TalonSRX driveRA = new TalonSRX( 1) {{ setNeutralMode(NeutralMode.Brake); }};
+	TalonSRX driveRB = new TalonSRX( 2) {{ setNeutralMode(NeutralMode.Brake); }};
+	TalonSRX driveRC = new TalonSRX( 3) {{ setNeutralMode(NeutralMode.Brake); }};
 
 	TalonSRX collector = new TalonSRX(0);
-	TalonSRX indexer = new TalonSRX(5);
+	TalonSRX indexer   = new TalonSRX(5);
 
 	// Sensors
-	AHRS navX = new AHRS(SPI.Port.kMXP, (byte) 50);
-	AnalogInput readSonar = new AnalogInput(1);
+	AHRS         navX       = new AHRS(SPI.Port.kMXP, (byte) 50);
+	AnalogInput  readSonar  = new AnalogInput(1);
 	DigitalInput ballSensor = new DigitalInput(7);
-	Encoder distanceL = new Encoder(0, 1, false, EncodingType.k1X);
-	Encoder distanceR = new Encoder(2, 3, true, EncodingType.k1X);
+	Encoder      distanceL  = new Encoder(0, 1, false, EncodingType.k1X);
+	Encoder      distanceR  = new Encoder(2, 3, true, EncodingType.k1X);
 	
 	Timer autoTimer = new Timer();
 
@@ -76,19 +44,19 @@ public class Robot extends TimedRobot {
 	Joystick driveStick = new Joystick(0);
 
 	// Global Variables
-	int autoStep = 0;
+	int autoStep    = 0;
 	int autoRoutine = 0;
 	
 
 	int autoLoopCounter = 0;
 
-	double turnSum = 0;
-	double lastOffYaw = 0;
-	boolean newPID = true;
-	double rampUpPower = 0;
+	double  turnSum     = 0;
+	double  lastOffYaw  = 0;
+	boolean newPID      = true;
+	double  rampUpPower = 0;
 
 	// GameData Stuff
-	String gameData = "";
+	String  gameData = "";
 	boolean switchLeft;
 	boolean scaleLeft;
 	boolean oppSwitchLeft;
@@ -101,27 +69,36 @@ public class Robot extends TimedRobot {
 	// PID Controllers
 	double straightP = 0.04, straightI = 0.0003, straightD = .01;
 	PIDCorrection driveStraightCorrection = new PIDCorrection();
-	PIDController driveStraight = new PIDController(straightP, straightI, straightD, navX, driveStraightCorrection,
-			0.020) {
-		{
-			setInputRange(-180.0, 180.0);
-			setOutputRange(-1.0, 1.0);
-			setContinuous();
-			disable();
-		}
-	};
+	PIDController driveStraight = new PIDController(
+		straightP,
+		straightI,
+		straightD,
+		navX,
+		driveStraightCorrection,
+		0.020
+	) {{
+		setInputRange(-180.0, 180.0);
+		setOutputRange(-1.0, 1.0);
+		setContinuous();
+		disable();
+	}};
 
 	double turnP = 0.04, turnI = 0, turnD = 0.02;
 	PIDCorrection turnRobotCorrection = new PIDCorrection();
-	PIDController turnRobot = new PIDController(turnP, turnI, turnD, navX, turnRobotCorrection, 0.020) {
-		{
-			setInputRange(-180.0, 180.0);
-			setOutputRange(-0.6, 0.6);
-			setAbsoluteTolerance(3);
-			setContinuous();
-			disable();
-		}
-	};
+	PIDController turnRobot = new PIDController(
+		turnP,
+		turnI,
+		turnD,
+		navX,
+		turnRobotCorrection,
+		0.020
+	) {{
+		setInputRange(-180.0, 180.0);
+		setOutputRange(-0.6, 0.6);
+		setAbsoluteTolerance(3);
+		setContinuous();
+		disable();
+	}};
 
 	/**********
 	 * Global *
@@ -139,8 +116,8 @@ public class Robot extends TimedRobot {
 	@Override
 	public void robotPeriodic() {
 		SmartDashboardUtil.dashboardPeriodic(this);
-		statusMessage = "Everything is good!"; // If this isn't still good when you print it again, we did something
-												// bad.
+		// If this isn't still good when you print it again, we did something bad.
+		statusMessage = "Everything is good!";
 	}
 
 	/************
@@ -179,9 +156,9 @@ public class Robot extends TimedRobot {
 
 	@Override
 	public void autonomousInit() {
-		gameData = DriverStation.getInstance().getGameSpecificMessage();
-		switchLeft = gameData.charAt(0) == 'L';
-		scaleLeft = gameData.charAt(0) == 'L';
+		gameData      = DriverStation.getInstance().getGameSpecificMessage();
+		switchLeft    = gameData.charAt(0) == 'L';
+		scaleLeft     = gameData.charAt(0) == 'L';
 		oppSwitchLeft = gameData.charAt(0) == 'L';
 
 		autoLoopCounter = 0;
@@ -212,14 +189,12 @@ public class Robot extends TimedRobot {
 		case 2:
 			RightSwitchThenCube.run(this);
 			break;
-		case 3:
+		//case 3:
 			// Right_Switch_Cube_Plus.run(this);
-			break;
+			//break;
 		case 4:
 			RightScaleSwitch.run(this);
-
 			break;
-
 		case 5:
 			GoStraightAutonomous.autoGoStraightTurnTest(this);
 			break;
@@ -237,7 +212,8 @@ public class Robot extends TimedRobot {
 
 	@Override
 	public void teleopInit() {
-		SmartDashboardUtil.getFromSmartDashboard(this); // force update
+		// force update
+		SmartDashboardUtil.getFromSmartDashboard(this);
 	}
 
 	@Override
