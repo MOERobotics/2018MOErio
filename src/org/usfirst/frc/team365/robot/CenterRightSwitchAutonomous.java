301a18530afb15784d068d46bcf66349f2844ef7
Audrey <ayang24.robin@gmail.com>
2018-02-28 19:09:11 -0500

    with autosimplify (not actually sure why this wants me to commit, but ok

--- src/org/usfirst/frc/team365/robot/CenterRightSwitchAutonomous.java
+++ src/org/usfirst/frc/team365/robot/CenterRightSwitchAutonomous.java
@@ -9,37 +9,37 @@ public class CenterRightSwitchAutonomous {
 	static void run(Robot robot) {
 		switch (robot.autoStep) {
 		case 1:
-			AutoSimplify.goStraight(robot, 2 * FEET, 0, .5);
+			robot.goStraight(2 * FEET, 0, .5);
 			break;
 		case 2:
-			AutoSimplify.turnToAngle(robot, 45);
+			robot.turnToAngle(45);
 			break;
 		case 3:
-			AutoSimplify.goStraight(robot, 72.7 * INCHES, 45, .5);
+			robot.goStraight(72.7 * INCHES, 45, .5);
 			break;
 		case 4:
-			AutoSimplify.turnToAngle(robot, 0);
+			robot.turnToAngle(0);
 			break;
 		case 5:
-			AutoSimplify.goStraight(robot, 9 * INCHES, 0, .5);
+			robot.goStraight(9 * INCHES, 0, .5);
 			break;
 		case 6:
-			AutoSimplify.pause(robot, 1.0);
+			robot.pause(1.0);
 			break;
 		case 7:
-			AutoSimplify.goStraight(robot, 48 * INCHES, 0, -.5);
+			robot.goStraight(48 * INCHES, 0, -.5);
 			break;
 		case 8:
-			AutoSimplify.turnToAngle(robot, -90);
+			robot.turnToAngle(-90);
 			break;
 		case 9:
-			AutoSimplify.goStraight(robot, 58 * INCHES, -90, .4);
+			robot.goStraight(58 * INCHES, -90, .4);
 			break;
 		case 10:
-			AutoSimplify.turnToAngle(robot, 0);
+			robot.turnToAngle(0);
 			break;
 		case 11:
-			AutoSimplify.goStraight(robot, 9 * INCHES, 0, .5);
+			robot.goStraight(9 * INCHES, 0, .5);
 			break;
 		}
 	}
--- src/org/usfirst/frc/team365/robot/RightSwitchThenCube.java
+++ src/org/usfirst/frc/team365/robot/RightSwitchThenCube.java
@@ -8,38 +8,38 @@ public class RightSwitchThenCube {
 		switch(robot.autoStep) {
 		case 1:
 			//goStraight(encoderTicks, navx setPoint, startPower - forwards/backwards is pos/neg)
-			AutoSimplify.goStraight(robot, 11 * FEET + 4 * INCHES, 0, .7);
+			robot.goStraight(11 * FEET + 4 * INCHES, 0, .7);
 			break;
 		case 2:
 			//turnToAngle(navxsetPoint, maxPower)
-			AutoSimplify.turnToAngle(robot, -90);
+			robot.turnToAngle(-90);
 			break;
 		case 3:
-			AutoSimplify.goStraight(robot, .5 * FEET, -90, .5);
+			robot.goStraight(.5 * FEET, -90, .5);
 			break;
 		case 4:
-			AutoSimplify.pause(robot, 1);
+			robot.pause(1);
 			break;
 		case 5:
-			AutoSimplify.goStraight(robot, .5 * FEET, -90, -.5);
+			robot.goStraight(.5 * FEET, -90, -.5);
 			break;
 		case 6:
-			AutoSimplify.turnToAngle(robot, 0);
+			robot.turnToAngle(0);
 			break;
 		case 7:
-			AutoSimplify.goStraight(robot, 6 * FEET, 0, .5);
+			robot.goStraight(6 * FEET, 0, .5);
 			break;
 		case 8:
-			AutoSimplify.turnToAngle(robot, -90);
+			robot.turnToAngle(-90);
 			break;
 		case 9:
-			AutoSimplify.goStraight(robot, 4 * FEET, -90, .5);
+			robot.goStraight(4 * FEET, -90, .5);
 			break;
 		case 10:
-			AutoSimplify.turnToAngle(robot, -180);
+			robot.turnToAngle(-180);
 			break;
 		case 11:
-			AutoSimplify.goStraight(robot, 1 * FEET, -180, .5);
+			robot.goStraight(1 * FEET, -180, .5);
 			break;
 		case 12:
 			robot.driveRobot(0,0);
