9a238c5a0bb5a3ddc5ec9c29cdf61957d30b236a
TheTripleV <vasistavovveti@gmail.com>
2018-03-08 19:48:41 -0500

    lefty

---
+++ src/org/usfirst/frc/team365/robot/LeftSwitchThenCube.java
@@ -0,0 +1,61 @@
+package org.usfirst.frc.team365.robot;
+
+public class LeftSwitchThenCube {
+	public static final double INCHES = Robot.INCHES_TO_ENCTICKS;
+	public static final double FEET = 12 * INCHES;
+	
+	static void run(Robot robot) {
+		switch(robot.autoStep) {
+		case 1:
+			//goStraight(encoderTicks, navx setPoint, startPower - forwards/backwards is pos/neg)
+			AutoSimplify.deployGrabber(robot);
+			AutoSimplify.goStraight(robot, 11 * FEET, 0, .7);
+			
+			if(robot.getEncoderMax() > 4 * FEET) {
+				AutoSimplify.raiseElevator(robot, 2000);
+			}
+			
+			break;
+		case 2:
+			//turnToAngle(navxsetPoint, maxPower)
+			AutoSimplify.raiseElevator(robot, 2000);
+			AutoSimplify.turnToAngle(robot, 90);
+			break;
+		case 3:
+			AutoSimplify.goStraight(robot, .5 * FEET, 90, .5);
+			break;
+		case 4:
+			AutoSimplify.launchCube(robot);
+			break;
+		case 5:
+			AutoSimplify.goStraight(robot, .5 * FEET, 90, -.5);
+			break;
+		case 6:
+			AutoSimplify.turnToAngle(robot, -180);
+			break;
+		case 7:
+			AutoSimplify.goStraight(robot, 6 * FEET, -180, -.5);
+			AutoSimplify.lowerElevator(robot, 0);
+			break;
+		case 8:
+			AutoSimplify.turnToAngle(robot, -215);
+			robot.cubeClawOpen();
+			break;
+		case 9:
+			AutoSimplify.goStraight(robot, 4 * FEET, -215, .5);
+			break;
+		case 10:
+			AutoSimplify.grabCube(robot);
+			break;
+		default:
+			robot.driveRobot(0,0);
+			robot.driveWrist(0);
+			robot.driveRoll(0);
+			robot.driveElevator(0);
+			break;
+			
+		}
+		
+		
+	}
+}
