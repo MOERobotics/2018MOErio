88595ae170953efbec6b9ce8d8f23de3f015e577
Louminator <rossi@math.udel.edu>
2018-03-16 10:04:44 +1100

    stripped down code for speed benchmarking ... straight

--- src/org/usfirst/frc/team365/robot/RightLeftScaleCube.java
+++ src/org/usfirst/frc/team365/robot/RightLeftScaleCube.java
@@ -7,30 +7,17 @@ public class RightLeftScaleCube {
 	
 	static void rightStart(Robot us)
 	{
-		/**
-		 * Forward: 210 inches
-		 * Left (-90)
-		 * Forward: 185 inches
-		 * Right (0)
-		 * Forward: 39 inches 
-		 * Backwards (0)
-		 * Backward: 37 inches
-		 * Turn (180)
-		 * Forward: 14 inches
-		 * 
-		 */
-		
 		switch (us.autoStep)
 		{
 		case 1: 
-			AutoSimplify.goStraight(us, 16.666 * FEET, 0, 0.7); //206.173 in
-			AutoSimplify.deployGrabber(us);
+			AutoSimplify.goStraight(us, 16* FEET, 0, 0.7);
+			// AutoSimplify.deployGrabber(us);
 			break;
-		case 2:
+/*		case 2:
 			AutoSimplify.autoPIDTurn(us, -90);
 			AutoSimplify.deployGrabber(us);
-			break;
-		case 3:
+			break;*/
+/*		case 3:
 			AutoSimplify.goStraight(us, 14.727 * FEET, -90, 0.5); //176.727 in
 			AutoSimplify.raiseElevator(us, us.HEIGHT_FOR_SCALE);
 			break;
@@ -56,8 +43,8 @@ public class RightLeftScaleCube {
 		case 9:
 			AutoSimplify.goStraight(us, 0 * FEET, 180, 0.4); //13.636 in
 			AutoSimplify.lowerElevator(us, us.BOTTOM_HEIGHT);
-			break;
-		case 10:
+			break;*/
+		case 2:
 			us.driveRobot(0, 0);
 			break;	
 		}
