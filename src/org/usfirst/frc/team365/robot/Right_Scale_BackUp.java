bee50583c09ce87057f9c64d3400090d6a5d01df
rohan <rohan@172.20.10.2>
2018-04-12 19:48:35 -0400

    Modified Back up program

--- src/org/usfirst/frc/team365/robot/Right_Scale_BackUp.java
+++ src/org/usfirst/frc/team365/robot/Right_Scale_BackUp.java
@@ -39,4 +39,55 @@ public class Right_Scale_BackUp {
 			break;
 		}
 	}
+	/*
+case 1:
+	AutoSimplify.raiseElevator(robot, robot.HEIGHT_FOR_SCALE);
+	AutoSimplify.autoPIDStraight(robot, 15.75 * FEET, 0, 0.6);
+//	AutoSimplify.goStraight(robot, 15.75*FEET, 0, 0.6);
+	AutoSimplify.deployGrabber(robot);
+	//ROBOT GOES STRAIGHT
+	//ELEVATOR UP
+	break;
+
+case 2:
+	AutoSimplify.raiseElevator(robot,robot.HEIGHT_FOR_SCALE);
+	AutoSimplify.autoPIDTurn(robot, -25);
+	AutoSimplify.deployGrabber(robot);
+	//ROBOT TURNS AND GRABBER LOWERS
+	break;
+
+case 3:
+	AutoSimplify.raiseElevator(robot,robot.HEIGHT_FOR_SCALE);
+	AutoSimplify.goStraight(robot, 4.083*FEET, -25, 0.4);
+	// ROBOT GOES STRAIGHT
+	break;
+
+case 4:
+	AutoSimplify.raiseElevator(robot,robot.HEIGHT_FOR_SCALE);
+	AutoSimplify.launchCube(robot);
+	//GRABBERS LAUNCH CUBE ONTO SWITCH
+	break;
+case 5: 
+	AutoSimplify.lowerElevator(robot,robot.BOTTOM_HEIGHT);
+	AutoSimplify.goStraight(robot, 3*FEET, -25, -0.4);
+	//ELEVATOR DOWN AND MOVING BACKWARDS
+
+	break;
+case 6:
+	AutoSimplify.lowerElevator(robot,robot.BOTTOM_HEIGHT);
+	robot.driveRobot(0, 0);
+	break;
+case 7: 
+	AutoSimplify.lowerElevator(robot,robot.BOTTOM_HEIGHT);
+	robot.driveRobot(0, 0);
+	break;
+case 8: 
+	AutoSimplify.lowerElevator(robot,robot.BOTTOM_HEIGHT);
+	robot.driveRobot(0, 0);
+	break;
+case 9: 
+	AutoSimplify.lowerElevator(robot,robot.BOTTOM_HEIGHT);
+	robot.driveRobot(0, 0);
+	break;
+	*/
 }
