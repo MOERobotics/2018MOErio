91d326cbb0a756d759f9405ababe49466e9ed10f
TheTripleV <vasistavovveti@gmail.com>
2018-05-20 21:23:00 -0400

    Workaround - top limit switch
    Elevator top limit has been changed from the limit switch to an encoder
    value of 5500 (just above scale height)

--- src/org/usfirst/frc/team365/robot/Robot.java
+++ src/org/usfirst/frc/team365/robot/Robot.java
@@ -575,7 +575,7 @@ public class Robot extends TimedRobot {
 		if(elevatorBottomLimitSwitch.get() && bottom) { //Drive positive
 			if(power < 0) power = 0;
 		}
-		else if(elevatorTopLimitSwitch.get() && top) {//Drive negative
+		else if(encoderElevator.getRaw() > 5500 && top) {//Drive negative
 			if(power > 0) power = 0;
 		}
 		else if(power < backDrive && power > -0.005) {
